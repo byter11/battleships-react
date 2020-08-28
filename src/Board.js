5c0f7702a4ae8da392e5e81261d6d794fc096ae9
byter11 <mohsinshk4026@gmail.com>
2020-08-28 20:42:34 +0500

    .

--- src/Board.js
+++ src/Board.js
@@ -1,24 +1,30 @@
 import React, {useState} from 'react';
-
+import Ship from './Ship.js';
 function Board(props){
+  
   const getshipIndexes = () => {
-    let tiles = [];
+    console.log('getship');
     let shipIndexes = [];
     const shipSizes = [1,3,3,4,5];
     for(let i=0; i<5; i++){
       const index = Math.floor( Math.random() * 100);
       const isVertical = Math.random() > 0.5;
       const coord = isVertical ? index/10 : index%10;
-      const isReverse = coord + shipSizes[i] > 10;
-      const indexes = Array.from(Array(shipsizes[i]).fill().map( (_,i) => return i));
+      const isReverse = coord + shipSizes[i] > 11;
+      const multiplier = isVertical ? isReverse ? (-10) : 10 : isReverse? -1 : 1
+      const indexes = Array.from(Array(shipSizes[i]).fill().map( (_,i) => index+(i*multiplier)));
+      console.log(i+ ' ' + index + ' ' + indexes)
       if(indexes.some( i => shipIndexes.includes(i)) ){
         i--;
+        console.log(shipIndexes + ' includes ' + indexes);
         continue;
       }
-      shipIndexes.push(indexes);
+      shipIndexes.push(...indexes);
     }
+    console.log('final ' + shipIndexes)
     return shipIndexes;
   }
+  
   
   const getTiles = (ships) =>{
     let tiles = [];
@@ -26,7 +32,7 @@ function Board(props){
       if (!ships.includes(i))
         tiles.push(<Tile
 		          key={i}
-		          onClick=props.handleClick/>)
+		          />)
 		  else
 		    tiles.push(<Ship
 		                  
@@ -34,19 +40,28 @@ function Board(props){
     }
     return tiles;
   }
-	const tiles = useState(getTiles(getshipIndexes()));
-	for(let i=0;i<100;i++)
-		tiles.push(<Tile
-		          key={i}
-		          onClick=props.handleClick/>)
-	return (<div id="board">
-				{tiles}
-			</div>);
+  const randomize = () =>{
+    const ships = getshipIndexes();
+    const tiles = getTiles(ships);
+    setTiles(tiles);
+  }
+	const [tiles, setTiles] = useState(getTiles(getshipIndexes()));
+	return (
+	      <div>
+	        <div className="board">
+	        {tiles}
+		  		</div>
+			  	<button onClick={randomize} style={{height: '20px', border: '1px solid black'}} ></button>
+			  </div>);
 }
 
 function Tile(props){
 	return(
-		<button id={"tile-"+props.id} className="tile"></button>
+		<button 
+		className="tile"
+	
+		>
+		</button>
 	)
 }
 
